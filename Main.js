53f6ebb38c43930dee88251d1c37ae7505ab3114
PAWEL\pawci <pawcio.lecz@gmail.com>
2023-04-04 15:06:06 +0200

    niedokonczona animacja

--- Main.js
+++ Main.js
@@ -14,3 +14,28 @@ function scrollFunction() {
 function goToTop() {
     document.documentElement.scrollTop = 0;
 }
+
+document.documentElement.scrollTop = 1;
+
+let us = document.getElementById("usTile");
+let usTop = us.getBoundingClientRect().top;
+
+let project = document.getElementById("projectTile");
+let projectTop = project.getBoundingClientRect().top;
+
+//let mosses = document.getElementById("mossesTile");
+//let mossesTop = mosses.getBoundingClientRect().top;
+
+var scroll = function() {
+  var y = document.documentElement.scrollTop;
+  console.log("y " + y)
+  console.log("pt " + projectTop)
+  if (y < projectTop) {
+    project.style.visibility = "visible";
+  }
+  if (y < usTop + 50) {
+    us.style.visibility = "visible";
+  }
+};
+
+window.addEventListener("scroll", scroll);
