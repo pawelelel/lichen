1befe638b0f19d248d58dc641648f3dfd5e2c719
PAWEL\pawci <pawel.leczkowski2005@gmail.com>
2023-12-29 23:14:09 +0100

    animations

--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,45 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+const elements = document.getElementsByClassName("hidden");
+
+for (let i = 0; i < elements.length; i++) {
+    observer.observe(elements[i])
+}
+
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function goToTop() {
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+
 let mybutton = document.getElementById("topBtn");
 let html = document.getElementById("html")
 
@@ -95,12 +137,14 @@ var scrollM = function scrollLichens() {
 
     return isVisible;
 }
+*/
+
 /*
 window.addEventListener("scroll", scrollU);
 window.addEventListener("scroll", scrollP);
 window.addEventListener("scroll", scrollM);
 */
-
+/*
 var percentScroll = function percentScroll(element) {
     const elementBounds = element.getBoundingClientRect();
     const viewportHeight = window.innerHeight;
@@ -156,3 +200,4 @@ window.addEventListener("scroll", selectedScroll);
 
 //const element = document.getElementById('projectTile');
 //window.addEventListener("scroll", (evt) => percentScroll(element));
+*/
