61945229285a27e523d80fe17831d1ce77a65887
PAWEL\pawci <pawel.leczkowski2005@gmail.com>
2023-12-30 15:56:36 +0100

    scroll + nav?

--- js/Main.js
+++ js/Main.js
@@ -2,7 +2,6 @@ document.documentElement.scrollTop = 0;
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
